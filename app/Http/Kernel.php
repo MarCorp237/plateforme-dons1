98265d5c731a08b012af26c3e5338632ed3e0a8a
MarCorp237 <marcorp237@gmail.com>
2026-04-01 04:46:52 +0200

    Fix: Add proper Laravel 10 bootstrap and kernel files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
-
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
